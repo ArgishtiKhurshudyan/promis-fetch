19bdaa8f3bde408ff3b9218492e27dc4e79884d5
ArgishtiKhurshudyan <argishtikhurshudyan@gmail.com>
2022-02-06 00:44:30 +0400

    feat: add fetch

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 let root = document.querySelector(".root");
+fetch("https://restcountries.com/v3.1/all")
+  .then(res => {
+    return res.json();
+  })
+  .then(data => {
+    getData(data);
+  });
+
+  
