e0ad21731c2d85d48fbeb1ee365548ed4ba6eece
ArgishtiKhurshudyan <argishtikhurshudyan@gmail.com>
2022-02-06 00:45:37 +0400

    feat: add function is name getData()

--- js/index.js
+++ js/index.js
@@ -8,3 +8,16 @@ fetch("https://restcountries.com/v3.1/all")
   });
 
   
+function getData(data) {
+    let countryName = data.map(item => item.name.common);
+    let flags = data.map(item => item.flags.png);
+    for (let i = 0; i < flags.length && i < countryName.length; i++) {
+      let img = document.createElement("img");
+      let div1 = document.createElement("div");
+      div1.className = "div-img";
+      div1.textContent = countryName[i];
+      div1.appendChild(img);
+      img.src += flags[i];
+      root.appendChild(div1);
+    }
+  }
